d14c0381ab33c9eaddf3c10588d27b02920f2537
jamesky <jamesky@github.com>
2026-05-07 05:19:01 +0800

    Clear music folder and change title to Jamesky's Blog

--- src/config.ts
+++ src/config.ts
@@ -24,7 +24,7 @@ import { LinkPreset } from "./types/config";
 const SITE_LANG = "en"; // 语言代码，例如：'en', 'zh_CN', 'ja' 等。
 const SITE_TIMEZONE = 8; //设置你的网站时区 from -12 to 12 default in UTC+8
 export const siteConfig: SiteConfig = {
-	title: "碎屑",
+	title: "Jamesky's Blog",
 	subtitle: "Fragment - 个人空间",
 	siteURL: "https://jameskyzx.github.io/Jamesky/", // 请替换为你的站点URL，以斜杠结尾
 	siteStartDate: "2025-01-01", // 站点开始运行日期，用于站点统计组件计算运行天数
