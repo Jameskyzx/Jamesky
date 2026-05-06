f24cf9adeb2908e1412f0c486355f29cd3920ace
jamesky <jamesky@github.com>
2026-05-07 04:41:32 +0800

    Switch to Mizuki template with Astro

--- next.config.mjs
+++
@@ -1,9 +0,0 @@
-/** @type {import('next').NextConfig} */
-const nextConfig = {
-  pageExtensions: ['js', 'jsx', 'ts', 'tsx', 'md', 'mdx'],
-  output: 'export',
-  basePath: '/Jamesky',
-  trailingSlash: true,
-}
-
-export default nextConfig
